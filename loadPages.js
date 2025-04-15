8fcc1b8fa521b3147271b246b7e65fb2add7b3db
MarcinGarcin <knucias123@gmail.com>
2025-04-15 12:47:46 +0200

    Avoiding corse restrictions

--- loadPages.js
+++ loadPages.js
@@ -1,4 +1,14 @@
 console.log("Loading pages initialized");
-fetch("https://tapthatball.onrender.com/");
-fetch("https://learningcenter.onrender.com/");
-fetch("https://mathtrainer.onrender.com/");
+
+const urls = [
+    "https://tapthatball.onrender.com/",
+    "https://learningcenter.onrender.com/",
+    "https://mathtrainer.onrender.com/"
+];
+
+urls.forEach(url => {
+    const iframe = document.createElement("iframe");
+    iframe.style.display = "none";
+    iframe.src = url;
+    document.body.appendChild(iframe);
+});
